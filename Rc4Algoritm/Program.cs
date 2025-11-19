257f2f281c0f5a0f4f7b1aae3c4a53c9da057fea
1Ruslan3 <keshabyn@mail.ru>
2025-11-19 17:33:50 +0300

    RC4

---
+++ Rc4Algoritm/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Rc4Algoritm;
+
+public sealed class Program
+{
+    public static void Main()
+    {
+        byte[] data = System.Text.Encoding.UTF8.GetBytes("Sqwoz bab");
+        byte[] key  = System.Text.Encoding.UTF8.GetBytes("secret");
+
+        var rc4 = new RC4(key);
+
+        rc4.ProcessInPlace(data); 
+        Console.WriteLine("Encrypted: " + BitConverter.ToString(data).Replace("-", ""));
+
+        var rc4dec = new RC4(key);
+        
+        rc4dec.ProcessInPlace(data); 
+        Console.WriteLine("Decrypted: " + System.Text.Encoding.UTF8.GetString(data));
+    }
+}
---
+++ Rc4Algoritm/Rc4Cipher.cs
@@ -0,0 +1,67 @@
+namespace Rc4Algoritm;
+
+public class RC4
+{
+    private readonly byte[] S = new byte[256];
+    private int x = 0;
+    private int y = 0;
+
+    public RC4(byte[] key)
+    {
+        Initialize(key);
+    }
+
+    private void Initialize(byte[] key)
+    {
+        for (int i = 0; i < 256; i++)
+            S[i] = (byte)i;
+
+        int j = 0;
+
+        for (int i = 0; i < 256; i++)
+        {
+            j = (j + S[i] + key[i % key.Length]) & 255;
+            Swap(i, j);
+        }
+    }
+
+    private void Swap(int i, int j)
+    {
+        byte t = S[i];
+        S[i] = S[j];
+        S[j] = t;
+    }
+
+    private byte NextByte()
+    {
+        x = (x + 1) & 255;
+        y = (y + S[x]) & 255;
+        Swap(x, y);
+        return S[(S[x] + S[y]) & 255];
+    }
+
+    public void InPlace(Span<byte> buffer)
+    {
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            buffer[i] ^= NextByte();
+        }
+    }
+
+    public async Task ProcessAsync(
+        Stream input,
+        Stream output,
+        int bufferSize = 8192,
+        CancellationToken cancellationToken = default)
+    {
+        byte[] buffer = new byte[bufferSize];
+        int bytesRead;
+
+        while ((bytesRead = await input.ReadAsync(buffer, 0, buffer.Length, cancellationToken)) > 0)
+        {
+            InPlace(buffer.AsSpan(0, bytesRead));
+
+            await output.WriteAsync(buffer, 0, bytesRead, cancellationToken);
+        }
+    }
+}
